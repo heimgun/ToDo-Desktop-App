8e976d232ed452d11d3d4e4af7326845184a0af7
heimgun <heidi.guneriussen@live.no>
2020-09-18 11:13:35 +0200

    CreateOrder done

--- src/main/java/CreateOrder.java
+++ src/main/java/CreateOrder.java
@@ -2,7 +2,9 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
@@ -15,6 +17,8 @@ public class CreateOrder {
     Connection con;
     PreparedStatement submitOrder;
 
+    @FXML
+    TextArea description;
 
 
     @FXML
@@ -25,6 +29,8 @@ public class CreateOrder {
 
     ObservableList<Customer> customers = FXCollections.observableArrayList(Customer.customers);
     ObservableList<Operator> operator = FXCollections.observableArrayList(Operator.operators);
+
+    Alert a = new Alert(Alert.AlertType.NONE);
 
 
     @FXML
@@ -47,11 +53,53 @@ public class CreateOrder {
 
 
 
-
-
     public void placeOrderButtonClicked(){
 
-       //Send Order to DB/API --> notification to Operator
+        Customer customer = customerList.getSelectionModel().getSelectedItem();
+        Operator operator = operatorList.getSelectionModel().getSelectedItem();
+
+        int operatorID = operator.getOperatorID();
+        int customerID = customer.getCustomerID();
+
+        String orderDescription = description.getText();
+
+
+        try{
+
+            submitOrderToDB(orderDescription, operatorID, customerID);
+            a.setAlertType(Alert.AlertType.CONFIRMATION);
+            a.setTitle("Order Submitted");
+            a.setContentText("Task " + orderDescription + " at " + customer.getName() + "s place\n" + "is sent to: "+ operator.getOperatorLastName());
+            a.show();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+   public void submitOrderToDB(String description, int operatorID, int customerID) throws SQLException {
+
+        con = ConDB.getConnection();
+        con.setAutoCommit(false);
+
+        submitOrder = con.prepareStatement("INSERT INTO \"Task\"" +
+                "(\"Description\", \"OperatorID\", \"CustomerID\", \"Status\", \"Accepted\")" +
+                "VALUES(?, ?, ?, ?, ?)");
+        submitOrder.setString(1, description);
+        submitOrder.setInt(2, operatorID);
+        submitOrder.setInt(3, customerID);
+        submitOrder.setString(4, "PENDING");
+        submitOrder.setString(5, "N");
+        submitOrder.executeUpdate();
+        con.commit();
+        submitOrder.close();
+
+       System.out.println("Order Submitted to Task in DB");
+
 
    }
 
